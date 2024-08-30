1e1c11c58681e32aebaf84e2b34148a5d0430899
unknown <abdullahsunasara6@gmail.com>
2024-08-30 11:24:08 +0530

    JWT Error handling, User_bp, pagination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from extensions import db, jwt
 from auth import auth_bp
+from users import user_bp
 def create_app():
 
     app = Flask(__name__)
@@ -12,5 +13,38 @@ def create_app():
     jwt.init_app(app)
     #register blueprint
     app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(user_bp, url_prefix='/users')
+
+    # Jwt error handling
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "Message" : "The token has expired",
+                    "error" : "token_expired"
+                }
+            ),401
+        )
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),401
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ), 401
+        )
 
     return app
